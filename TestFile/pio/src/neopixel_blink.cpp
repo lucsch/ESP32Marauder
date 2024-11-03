3eaf55c135954370ccce835661812bf802805373
Lucien <lucien.schreiber@terranum.ch>
2024-11-03 16:12:50 +0100

    Adding code for testing RGB led

---
+++ TestFile/pio/src/neopixel_blink.cpp
@@ -0,0 +1,28 @@
+//
+// Created by Lucien Schreiber on 03.11.2024.
+//
+#include "Arduino.h"
+#include "Adafruit_NeoPixel.h"
+
+#define NEOPIXEL_PIN 25
+
+Adafruit_NeoPixel strip = Adafruit_NeoPixel(1, NEOPIXEL_PIN, NEO_GRB + NEO_KHZ800);
+
+
+void setup() {
+    strip.begin();
+    strip.show();
+    strip.setBrightness(50);
+}
+
+void loop() {
+    strip.fill(strip.Color(255, 0, 0));
+    strip.show();
+    delay(500);
+    strip.fill(strip.Color(0, 255, 0));
+    strip.show();
+    delay(500);
+    strip.fill(strip.Color(0, 0, 255));
+    strip.show();
+    delay(500);
+}
