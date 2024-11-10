067e0313dbb90a19bca4eaa1f80a6ca309814e17
Lucien <lucien.schreiber@terranum.ch>
2024-11-10 12:35:33 +0100

    Adding same code for touch test on Marauder and CYD board

--- TestFile/pio/src/touch_test.cpp
+++ TestFile/pio/src/touch_test.cpp
@@ -1,30 +1,6 @@
-/*******************************************************************
-    A touch screen test for the ESP32 Cheap Yellow Display.
-
-    https://github.com/witnessmenow/ESP32-Cheap-Yellow-Display
-
-    If you find what I do useful and would like to support me,
-    please consider becoming a sponsor on Github
-    https://github.com/sponsors/witnessmenow/
-
-    Written by Brian Lough
-    YouTube: https://www.youtube.com/brianlough
-    Twitter: https://twitter.com/witnessmenow
- *******************************************************************/
-
-// Make sure to copy the UserSetup.h file into the library as
-// per the Github Instructions. The pins are defined in there.
-
-// ----------------------------
-// Standard Libraries
-// ----------------------------
+// This is the Touch Test for ESP32 Marauder
 
 #include <SPI.h>
-
-// ----------------------------
-// Additional Libraries - each one of these will need to be installed.
-// ----------------------------
-
 #include <XPT2046_Touchscreen.h>
 // A library for interfacing with the touch screen
 //
@@ -39,84 +15,114 @@
 
 
 // ----------------------------
-// Touch Screen pins
+// Touch Screen pins defined in platformio.ini
 // ----------------------------
 
 // The CYD touch uses some non default
 // SPI pins
 
+/*
+CYD IRQ 36 --> ESPMarauder IRQ is connected to VCC
+CYD MOSI 32 --> ESPMarauder MOSI (23)
+CYD MISO 39 --> ESPMarauder MISO (19)
+CYD CLK 25 --> ESPMarauder CLK  (18)
+CYD CS 33 --> ESPMarauder CHIP_SELECT   (21)
+*/
+#if defined(MARAUDER)
+// marauder use HSPI ports to communicate with touch driver and TFT display
+#define XPT2046_IRQ 255
+#define XPT2046_MOSI TFT_MOSI
+#define XPT2046_MISO TFT_MISO
+#define XPT2046_CLK TFT_SCLK
+#define XPT2046_CS TOUCH_CS
+#elif defined(CYD)
+// Cheap yellow display uses HSPI ports to communicate with TFT display and
+// VSPI ports to communicate with touch driver
+// see here : https://randomnerdtutorials.com/esp32-cheap-yellow-display-cyd-pinout-esp32-2432s028r/
 #define XPT2046_IRQ 36
 #define XPT2046_MOSI 32
 #define XPT2046_MISO 39
 #define XPT2046_CLK 25
 #define XPT2046_CS 33
-
-// ----------------------------
+#else
+#error "Please define MARAUDER or CYD"
+#endif
 
 SPIClass mySpi = SPIClass(VSPI);
 XPT2046_Touchscreen ts(XPT2046_CS, XPT2046_IRQ);
-
 TFT_eSPI tft = TFT_eSPI();
 
 void setup() {
-  Serial.begin(115200);
+    Serial.begin(115200);
 
-  // Start the SPI for the touch screen and init the TS library
-  mySpi.begin(XPT2046_CLK, XPT2046_MISO, XPT2046_MOSI, XPT2046_CS);
-  ts.begin(mySpi);
-  ts.setRotation(1);
+#if defined(MARAUDER)
+    pinMode(TFT_BL, OUTPUT);
+    digitalWrite(TFT_BL, LOW); // backlight OFF
+    // Preset SPI CS pins to avoid bus conflicts
+    digitalWrite(TFT_CS, HIGH);
+#endif
 
-  // Start the tft display and set it to black
-  tft.init();
-  tft.setRotation(1); //This is the display in landscape
+    // Start the SPI for the touch screen and init the TS library
+    mySpi.begin(XPT2046_CLK, XPT2046_MISO, XPT2046_MOSI, XPT2046_CS);
+    ts.begin(mySpi);
+    ts.setRotation(1);
 
-  // Clear the screen before writing to it
-  tft.fillScreen(TFT_BLACK);
+    // Start the tft display and set it to black
+    tft.init();
+    tft.setRotation(1); //This is the display in landscape
 
-  int x = 320 / 2; // center of display
-  int y = 100;
-  int fontSize = 2;
-  tft.drawCentreString("Touch Screen to Start", x, y, fontSize);
+    // Clear the screen before writing to it
+    tft.fillScreen(TFT_BLACK);
+    tft.setTextColor(TFT_WHITE, TFT_BLACK);
+
+    int x = 320 / 2; // center of display
+    int y = 100;
+    int fontSize = 2;
+    tft.drawCentreString("Touch Screen to Start", x, y, fontSize);
+
+#if defined(MARAUDER)
+    digitalWrite(TFT_BL, HIGH);
+#endif
 }
 
 void printTouchToSerial(TS_Point p) {
-  Serial.print("Pressure = ");
-  Serial.print(p.z);
-  Serial.print(", x = ");
-  Serial.print(p.x);
-  Serial.print(", y = ");
-  Serial.print(p.y);
-  Serial.println();
+    Serial.print("Pressure = ");
+    Serial.print(p.z);
+    Serial.print(", x = ");
+    Serial.print(p.x);
+    Serial.print(", y = ");
+    Serial.print(p.y);
+    Serial.println();
 }
 
 void printTouchToDisplay(TS_Point p) {
 
-  // Clear screen first
-  tft.fillScreen(TFT_BLACK);
-  tft.setTextColor(TFT_WHITE, TFT_BLACK);
+    // Clear screen first
+    tft.fillScreen(TFT_BLACK);
+    tft.setTextColor(TFT_WHITE, TFT_BLACK);
 
-  int x = 320 / 2; // center of display
-  int y = 100;
-  int fontSize = 2;
+    int x = 320 / 2; // center of display
+    int y = 100;
+    int fontSize = 2;
 
-  String temp = "Pressure = " + String(p.z);
-  tft.drawCentreString(temp, x, y, fontSize);
+    String temp = "Pressure = " + String(p.z);
+    tft.drawCentreString(temp, x, y, fontSize);
 
-  y += 16;
-  temp = "X = " + String(p.x);
-  tft.drawCentreString(temp, x, y, fontSize);
+    y += 16;
+    temp = "X = " + String(p.x);
+    tft.drawCentreString(temp, x, y, fontSize);
 
-  y += 16;
-  temp = "Y = " + String(p.y);
-  tft.drawCentreString(temp, x, y, fontSize);
+    y += 16;
+    temp = "Y = " + String(p.y);
+    tft.drawCentreString(temp, x, y, fontSize);
 }
 
 void loop() {
-  if (ts.tirqTouched() && ts.touched()) {
-    TS_Point p = ts.getPoint();
-    printTouchToSerial(p);
-    printTouchToDisplay(p);
-    delay(100);
+    if (ts.tirqTouched() && ts.touched()) {
+        TS_Point p = ts.getPoint();
+        printTouchToSerial(p);
+        printTouchToDisplay(p);
+        delay(100);
 
-  }
+    }
 }
